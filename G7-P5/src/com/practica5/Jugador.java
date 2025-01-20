eb5e48970b487ed5f625f02f9de53e80115c9954
Maël Gruand <maelgruand1@yahoo.com>
2025-01-20 14:37:06 +0100

    creating enum position

--- G7-P5/src/com/practica5/Jugador.java
+++ G7-P5/src/com/practica5/Jugador.java
@@ -1,5 +1,13 @@
 package com.practica5;
+import java.util.Date;
 
 public class Jugador {
-    
+   private String nombre;
+   private Date fechaNacimiento;
+   private String pais;
+   private int dorsal;
+   private String poticion;
 }
+
+
+
