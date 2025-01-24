d5a8c4a34e69d657e84d6e82aeaafbdd69daa0af
Maël Gruand <maelgruand1@yahoo.com>
2025-01-24 10:58:44 +0100

    add funcionality

--- G7-P5/src/com/practica5/Main.java
+++ G7-P5/src/com/practica5/Main.java
@@ -11,11 +11,12 @@ import com.practica5.Jugadores.Poticion;
 public class Main {
     public static void main(String[] args) throws Exception {
         Jugador jugador = new Jugador("Toni Kros", Date.valueOf("1990-01-04"), "Alemania", 8, Poticion.centro);
-        Jugador jugador1 = new Jugador("Cristiano Ronaldo", Date.valueOf("1985-02-05"), "Portugal", 7, Poticion.delentero);
-        
+        Jugador jugador1 = new Jugador("Cristiano Ronaldo", Date.valueOf("1985-02-05"), "Portugal", 7,
+                Poticion.delentero);
+
         Entrenador entrenador = new Entrenador("Zinedine Zidane", Formacion._433);
         Presidente presidente = new Presidente(456789, "Florentino Perez", null);
-        
+
         ArrayList<Jugador> realMadrid = new ArrayList<Jugador>();
         realMadrid.add(jugador);
         realMadrid.add(jugador1);
