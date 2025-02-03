bca762a0cbea854992fab4a81e3adade484440a3
Maël Gruand <maelgruand1@yahoo.com>
2025-02-03 10:58:19 +0100

    Corrigo errores

--- G7-P5/src/com/practica5/Presidente.java
+++ G7-P5/src/com/practica5/Presidente.java
@@ -1,29 +1,31 @@
 package com.practica5;
 
+import com.practica5.Jugadores.EstadoTraspaso;
+import com.practica5.Jugadores.Jugador;
+
 public class Presidente {
     private String nombre;
 
-    public Presidente(String nombre){
+    public Presidente(String nombre) {
         this.nombre = nombre;
     }
+
     public void decidirTraspaso(Jugador jugador, Equipo origen, Equipo destino, boolean aceptar) {
         if (jugador.getEstadoTraspaso() != EstadoTraspaso.APROBADO_POR_ENTRENADOR) {
             System.out.println("Error: El presidente solo puede decidir sobre jugadores aprobados por el entrenador.");
             return;
         }
-            if (aceptar) {
-                jugador.setEstadoTraspaso(EstadoTraspaso.APROBADO_POR_PRESIDENTE);
-                origen.removerJugador(jugador);
-                destino.agregarJugador(jugador);
+        if (aceptar) {
+            jugador.setEstadoTraspaso(EstadoTraspaso.APROBADO_POR_PRESIDENTE);
+            origen.removerJugador(jugador);
+            destino.agregarJugador(jugador);
 
-                System.out.println("Traspaso aprobado por el presidente " + nombre + 
-                ": " + jugador.getNombre() + " pasa de " + origen.getNombre() + " a " + destino.getNombre());
-            } else {
-                jugador.setEstadoTraspaso(EstadoTraspaso.RECHAZADO_POR_PRESIDENTE);
-                System.out.println("Traspaso rechazado por el presidente " + nombre + 
-                " para el jugador " + jugador.getNombre());
-            }
+            System.out.println("Traspaso aprobado por el presidente " + nombre +
+                    ": " + jugador.getNombre() + " pasa de " + origen.getNombre() + " a " + destino.getNombre());
+        } else {
+            jugador.setEstadoTraspaso(EstadoTraspaso.RECHAZADO_POR_PRESIDENTE);
+            System.out.println("Traspaso rechazado por el presidente " + nombre +
+                    " para el jugador " + jugador.getNombre());
         }
     }
-
-    
+}
